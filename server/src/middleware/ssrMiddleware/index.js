8ad51d5f6ce57434a054c0b49649619b645bd429
Calvin Lai <callai@gmail.com>
2018-08-13 10:28:20 -0400

    fix: get style tags from current rendered app

--- server/src/middleware/ssrMiddleware/index.js
+++ server/src/middleware/ssrMiddleware/index.js
@@ -117,7 +117,7 @@ export default function ssrMiddleware(options = {}) {
       const markup = render(html, {
         bundles: (stats && getBundles(stats, modules)) || [],
         state: client.extract(),
-        styles: opts.styles,
+        styles: sheet.getStyleTags(),
         manifest: webpackManifest,
         template
       });
